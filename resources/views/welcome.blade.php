b626c6156c1721ccc883378baee60f68f1e22b74
Pedro Falconi <pedrofalck2000@gmail.com>
2022-09-21 21:40:58 -0300

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.app')
-
 @section('content')
+@guest
 <x-login />
+@endguest
+@auth
+<a type="button" class="btn btn-primary" href="{{ route('contatos.index')}}">
+    Ver Contatos
+</a>
+@endauth
+@endsection
+@section('scripts')
+
+
 @endsection
