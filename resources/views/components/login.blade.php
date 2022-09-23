52154138621f4e4f7acdd63f844f63fd1d886b1a
Bruno Moreira <68245780+RaidersMns@users.noreply.github.com>
2022-09-22 21:38:51 -0300

    Att. Homepage

--- resources/views/components/login.blade.php
+++ resources/views/components/login.blade.php
@@ -1,6 +1,9 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+
+            <img src="{{ asset('img/logo.png')}}" class="col-md-4  offset-md-4 mb-3">
+
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
 
@@ -15,9 +18,9 @@
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -29,9 +32,9 @@
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -55,9 +58,9 @@
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
                                 @endif
                             </div>
                         </div>
